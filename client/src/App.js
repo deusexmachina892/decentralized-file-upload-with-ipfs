6f68366dd7c65fa35c84c3165b2470d1089a591e
deusexmachina892 <deusexmachina892@gmail.com>
2018-11-04 06:59:10 -0500

    Final steps

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,14 @@ import ipfs from './ipfs';
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { 
+    hash: '',
+    storageValue: 0, 
+    web3: null, 
+    accounts: null, 
+    contract: null,
+    buffer: null
+  };
 
   componentDidMount = async () => {
     try {
@@ -21,10 +28,10 @@ class App extends Component {
       const Contract = truffleContract(SimpleStorageContract);
       Contract.setProvider(web3.currentProvider);
       const instance = await Contract.deployed();
-
+      const hash = await instance.get.call({from: accounts[0]})
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance, ipfsHash: hash });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -42,28 +49,72 @@ class App extends Component {
 
     // Get the value from the contract to prove it worked.
     const response = await contract.get();
-
+    console.log(response);
     // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
   };
 
-  render() {
+  onSubmit = (e) =>{
+    e.preventDefault();
+    const {accounts, contract,ipfsHash} = this.state;
+    ipfs.files.add(this.state.buffer, async (error, result)=>{
+      if(error){
+          console.log(error);
+          return;
+      }
+      if(contract){
+        await contract.set(result[0].hash, {from: accounts[0]});
+        const response = await contract.get();
+        console.log(response);
+        this.setState({
+          ipfsHash : result[0].hash
+        })
+        console.log(this.state.ipfsHash); 
+      return;
+      }
+     
+    });
+  }
+  captureFile = (e) =>{
+    const file = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = ()=>{
+      this.setState({buffer: Buffer(reader.result)})
+      console.log('buffer', this.state.buffer)
+    }
+  }
+  checkContract = async () =>{
+    const {web3, contract} = this.state;
+      // let index;
+      let contractAddress = '0xc7E6875BC2675394e3d52B6749B9ED2878B8edBC'
+      // for (index = 0; index < 3; index++){
+      // console.log(`[${index}]` + 
+      //   web3.eth.getStorageAt(contractAddress, index))
+      // }
+    // const response = await contract.get();
+    // console.log(typeof(response));
+    // console.log(this.state.ipfsHash)
+
+    // const event = await contract.allEvents();
+    // const result = await contract.getPastEvents('allEvents')
+    console.log(this.state.ipfsHash)
+    console.log(contract)
+  }
+render () {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 37</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+            <h1>IPFS File Upload Dapp</h1>
+          <p>This image is sotred on the Ethereum Blockchain</p>
+          <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt="Hello" />  
+          <form onSubmit={this.onSubmit.bind(this)}>
+          <label htmlFor="imageUploader">Select an image</label>
+          <input type="file" onChange={this.captureFile.bind(this)} />
+          <input type="submit" />
+          </form>
+          <button onClick={this.checkContract.bind(this)}>Check Contract</button>
       </div>
     );
   }
